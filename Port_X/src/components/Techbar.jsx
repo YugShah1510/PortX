433884cdf541794ab809eb3ed71688a4832b532d
YugShah1510 <yugshah2905@gmail.com>
2026-05-03 17:28:37 +0530

    Added Techbar

---
+++ Port_X/src/components/Techbar.jsx
@@ -0,0 +1,38 @@
+import "./TechBar.css";
+
+const TechBar = () => {
+  return (
+    <div className="tech-wrapper">
+      <div className="tech-track">
+        {/* Repeat content twice for smooth infinite scroll */}
+        <div className="tech-content">
+          <span>◆ SVELTEKIT</span>
+          <span>◆ REACT</span>
+          <span>◆ NEXT.JS</span>
+          <span>◆ TAILWIND</span>
+          <span>◆ ASTRO</span>
+          <span>◆ FRAMER MOTION</span>
+          <span>◆ VANILLA JS</span>
+          <span>◆ THREE.JS</span>
+          <span>◆ TYPESCRIPT</span>
+          <span>◆ REMIX</span>
+        </div>
+
+        <div className="tech-content">
+          <span>◆ SVELTEKIT</span>
+          <span>◆ REACT</span>
+          <span>◆ NEXT.JS</span>
+          <span>◆ TAILWIND</span>
+          <span>◆ ASTRO</span>
+          <span>◆ FRAMER MOTION</span>
+          <span>◆ VANILLA JS</span>
+          <span>◆ THREE.JS</span>
+          <span>◆ TYPESCRIPT</span>
+          <span>◆ REMIX</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TechBar;
--- Port_X/src/pages/Home.jsx
+++ Port_X/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import TechBar from "../components/TechBar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <TechBar /> 
     </div>
   );
 };
