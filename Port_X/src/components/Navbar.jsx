b0db41e1be0af539b28562af87cb33d063df40e4
hetjaviya15 <hetjaviya08@gmail.com>
2026-05-05 23:13:48 +0530

    Log In Page Created

--- Port_X/src/components/Navbar.jsx
+++ Port_X/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../assets/Logo.jpeg"; // 👈 import
 
@@ -11,7 +12,7 @@ const Navbar = () => {
       <div className="nav-links">
         <a href="#">Home</a>
         <a href="#">Templates</a>
-        <a href="#">Log in</a>
+        <Link to="/login">Log in</Link>
       </div>
 
       <button className="preview-btn">Try Preview</button>
---
+++ Port_X/src/pages/Login.jsx
@@ -0,0 +1,7 @@
+import AuthPage from "../components/AuthPage";
+
+const Login = () => {
+  return <AuthPage />;
+};
+
+export default Login;
