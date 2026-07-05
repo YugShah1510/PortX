04b88471146a0d27f392707b1f1ab4b19e1cd608
Yug Shah <yugshah2905@gmail.com>
2026-07-05 11:43:05 +0530

    Update Techbar.jsx

--- Port_X/src/components/Techbar.jsx
+++ Port_X/src/components/Techbar.jsx
@@ -5,7 +5,7 @@ const TechBar = () => {
     <div className="tech-wrapper">
       <div className="tech-track">
 
-        <div className="tech-content">
+        <div className="tech-contnt">
           <span>◆ SVELTEKIT</span>
           <span>◆ REACT</span>
           <span>◆ NEXT.JS</span>
